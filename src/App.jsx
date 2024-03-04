00b331f463210edcb8bc7f54a797840823f061be
Thomas Tran <2001thomas.tran@gmail.com>
2024-03-04 00:27:04 -0600

    added components, added src features, added react-router-dom in packages

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import './App.css';
+import Header from './components/Header';
+import Project from './components/Project';
+import Footer from './components/Footer';
+
+function App() {
+    return(
+        <div className='portfolio-app'>
+            <Header />
+            <Project />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
