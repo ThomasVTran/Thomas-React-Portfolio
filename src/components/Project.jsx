de8d7d9cc801d78e7c5feeaecf48276cc9565cd8
Thomas Tran <2001thomas.tran@gmail.com>
2024-03-05 11:29:51 -0600

    switched Portfolio and Project item mapping

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,60 +1,26 @@
-import Portfolio from '../pages/Portfolio';
-import FreshTomatoes from '../images/FreshTomatoes-screenshot.png'
-import Fur from "../images/Fur-companion-pilot-screenshot.png";
-import Note from "../images/NoteTracker.png";
+import Card from 'react-bootstrap/Card';
 
-const projects =[
-    {
-        title: 'FreshTomatoes',
-        hub:    "ThomasVTran",
-        deployed:   "Heroku Deployment",
-        image:  FreshTomatoes,
-        deployedLink:   "https://secure-chamber-09606-709b0e7a0ed2.herokuapp.com/",
-        hubLink:  "https://github.com/ThomasVTran/movie-club?tab=readme-ov-file",
-    },
-    {
-        title: 'Fur-Companion-pilot',
-        hub:    "ThomasVTran",
-        deployed:   "",
-        image:  Fur,
-        deployedLink:   "",
-        hubLink:  "https://thomasvtran.github.io/fur-companion-pilot/",
-    },
-    {
-        title: 'Note Tracker',
-        hub:    "ThomasVTran",
-        deployed:   "Herokue Deployment",
-        image:  Note,
-        deployedLink:   "https://guarded-scrubland-98500-8a9de5484cb0.herokuapp.com/notes",
-        hubLink:  "https://github.com/ThomasVTran/Note-Tracker?tab=readme-ov-file",
-    },
-    {
-        title: 'Project 4',
-        hub:    "",
-        deployed:   "",
-        image:  "",
-        deployedLink:   "",
-        hubLink:  "",
-    },
-    {
-        title: 'Project 5',
-        hub:    "",
-        deployed:   "",
-        image:  "",
-        deployedLink:   "",
-        hubLink:  "",
-    },
-    {
-        title: 'Project 6',
-        hub:    "",
-        deployed:   "",
-        image:  "",
-        deployedLink:   "",
-        hubLink:  "",
-    },
-]
-function Project() {
-    return <Portfolio projects={projects}/>;
+function Project(props) {
+  return (
+    <div>
+      {props.projects.map(project => (
+        <p className='card' key={project.title}>
+          <Card className="bg-dark text-white" >
+            <Card.Img src={project.image} alt={project.title} />
+            <Card.ImgOverlay>
+              <Card.Title>{project.title}</Card.Title>
+              <Card.Text href={project.hubLink}>
+                {project.hub}
+              </Card.Text>
+              <Card.Text href={project.deployedLink}>
+                {project.deployed}
+              </Card.Text>
+            </Card.ImgOverlay>
+          </Card>
+        </p>
+      ))}
+    </div>
+  );
 }
 
 export default Project;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,25 +1,63 @@
-import Card from 'react-bootstrap/Card';
+import Project from '../components/Project';
+import FreshTomatoes from '../images/FreshTomatoes-screenshot.png'
+import Fur from "../images/Fur-companion-pilot-screenshot.png";
+import Note from "../images/NoteTracker.png";
 
-export default function Portfolio(props) {
+const projects =[
+    {
+        title:'FreshTomatoes',
+        hub:"ThomasVTran",
+        deployed:"Heroku Deployment",
+        image:FreshTomatoes,
+        deployedLink:"https://secure-chamber-09606-709b0e7a0ed2.herokuapp.com/",
+        hubLink:"https://github.com/ThomasVTran/movie-club?tab=readme-ov-file",
+    },
+    {
+        title:'Fur-Companion-pilot',
+        hub:"ThomasVTran",
+        deployed:"",
+        image:Fur,
+        deployedLink:"",
+        hubLink:"https://thomasvtran.github.io/fur-companion-pilot/",
+    },
+    {
+        title:'Note Tracker',
+        hub:"ThomasVTran",
+        deployed:"Herokue Deployment",
+        image:  Note,
+        deployedLink:"https://guarded-scrubland-98500-8a9de5484cb0.herokuapp.com/notes",
+        hubLink:"https://github.com/ThomasVTran/Note-Tracker?tab=readme-ov-file",
+    },
+    {
+        title:'Project 4',
+        hub:"",
+        deployed:"",
+        image:"",
+        deployedLink:"",
+        hubLink:"",
+    },
+    {
+        title:'Project 5',
+        hub:"",
+        deployed:"",
+        image:"",
+        deployedLink:"",
+        hubLink:"",
+    },
+    {
+        title:'Project 6',
+        hub:"",
+        deployed:"",
+        image:"",
+        deployedLink:"",
+        hubLink:"",
+    },
+]
+function Portfolio() {
   return (
-    <div>
-      <h1>Portfolio</h1>
-      <p className='card'>
-        {props.projects.map(project => (
-          <Card className="bg-dark text-white" key={project.title}>
-            <Card.Img src={project.image} alt={project.title}/>
-            <Card.ImgOverlay>
-              <Card.Title>{project.title}</Card.Title>
-              <Card.Text href={project.hubLink}>
-                {project.hub}
-              </Card.Text>
-              <Card.Text href={project.deployedLink}>
-                {project.deployed}
-              </Card.Text>
-            </Card.ImgOverlay>
-          </Card>
-        ))}
-      </p>
-    </div>
-  );
+      <h1> Portfolio </h1>,
+      <Project projects={projects}/>
+    );
 }
+
+export default Portfolio;
